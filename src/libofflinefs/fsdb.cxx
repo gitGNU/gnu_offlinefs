326188c33438715def60d7f94d1333c674a593ce
curro <currojerez@gmail.com>
2008-05-29 13:40:08 +0200

    License headers added on most files COPYING added, with the GNU GPL v3 modifications in README

--- src/libofflinefs/fsdb.cxx
+++ src/libofflinefs/fsdb.cxx
@@ -1,3 +1,19 @@
+//     Copyright (C) 2008 Francisco Jerez
+//     This file is part of offlinefs.
+
+//     offlinefs is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     offlinefs is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with offlinefs.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "fsdb.hxx"
 #include "fsnodes.hxx"
 #include "media.hxx"
